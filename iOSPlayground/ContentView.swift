3924c2713050ca054e1553fff48d39b7b695ab58
David Álvarez Rosa <dalvrosa@amazon.com>
2023-12-15 15:25:42 +0100

    Initial Commit

---
+++ iOSPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSPlayground
+//
+//  Created by Alvarez Rosa, David on 12/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSPlayground/iOSPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSPlaygroundApp.swift
+//  iOSPlayground
+//
+//  Created by Alvarez Rosa, David on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
